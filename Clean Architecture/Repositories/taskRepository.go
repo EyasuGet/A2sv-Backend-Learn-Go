50c709fa26fe3575a1be314996d1fb925cc5ebf4
Eyasu <eyasugetaneh0@gmail.com>
2025-07-29 00:18:39 +0300

    add task and user repository implementations

---
+++ Clean Architecture/Repositories/taskRepository.go
@@ -0,0 +1,146 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/EyasuGet/task-manager-mongo/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type taskRepository struct {
+    taskCollection *mongo.Collection
+}
+
+func NewTaskRepository(col *mongo.Collection) *taskRepository {
+    
+    return &taskRepository{taskCollection: col}
+}
+
+func (tr *taskRepository) GetAll() ([]domain.Task, error) {
+    cursor, err := tr.taskCollection.Find(context.TODO(), bson.D{})
+    if err != nil {
+        return nil, err
+    }
+    defer cursor.Close(context.TODO())
+
+    var tasks []domain.Task
+    for cursor.Next(context.TODO()) {
+        var taskDoc bson.M
+        err := cursor.Decode(&taskDoc)
+        if err != nil {
+            return nil, err
+        }
+
+        id, ok := taskDoc["_id"].(primitive.ObjectID)
+        if !ok {
+            return nil, errors.New("invalid ID type")
+        }
+
+        dueDate, ok := taskDoc["due_date"].(primitive.DateTime)
+        if !ok {
+            dueDate = primitive.DateTime(time.Now().Unix() * 1000)
+        }
+
+        task := domain.Task{
+            ID:          id.Hex(),
+            Title:       taskDoc["title"].(string),
+            Description: taskDoc["description"].(string),
+            DueDate:     dueDate.Time(),
+            Status: 		taskDoc["status"].(string),
+        }
+        tasks = append(tasks, task)
+    }
+
+    return tasks, nil
+}
+
+func (tr *taskRepository) GetByID(id string) (domain.Task, error) {
+    objectID, err := primitive.ObjectIDFromHex(id)
+    if err != nil {
+        return domain.Task{}, err
+    }
+
+    filter := bson.M{"_id": objectID}
+    var taskDoc bson.M
+    err = tr.taskCollection.FindOne(context.TODO(), filter).Decode(&taskDoc)
+    if err != nil {
+        return domain.Task{}, err
+    }
+
+    oid, ok := taskDoc["_id"].(primitive.ObjectID)
+    if !ok {
+        return domain.Task{}, errors.New("invalid ID type")
+    }
+
+    dueDate, ok := taskDoc["due_date"].(primitive.DateTime)
+    if !ok {
+        dueDate = primitive.DateTime(time.Now().Unix() * 1000) 
+    }
+
+    task := domain.Task{
+        ID:          oid.Hex(),
+        Title:       taskDoc["title"].(string),
+        Description: taskDoc["description"].(string),
+        DueDate:     dueDate.Time(),
+        Status:      taskDoc["status"].(string),
+    }
+    return task, nil
+}
+
+func (tr *taskRepository) Create(task domain.Task) (domain.Task, error) {
+    newID := primitive.NewObjectID()
+
+    doc := bson.M{
+        "_id":         newID,
+        "title":       task.Title,
+        "description": task.Description,
+        "due_date":    task.DueDate,
+        "status":      task.Status,
+    }
+
+    _, err := tr.taskCollection.InsertOne(context.TODO(), doc)
+    if err != nil {
+        return domain.Task{}, err
+    }
+
+    task.ID = newID.Hex()
+    return task, nil
+}
+
+func (tr *taskRepository) Update(id string, task domain.Task) (domain.Task, error) {
+    objectID, err := primitive.ObjectIDFromHex(id)
+    if err != nil {
+        return domain.Task{}, err
+    }
+
+    filter := bson.M{"_id": objectID}
+    update := bson.M{
+        "$set": bson.M{
+            "title":       task.Title,
+            "description": task.Description,
+            "due_date":    task.DueDate,
+            "status":      task.Status,
+        },
+    }
+
+    _, err = tr.taskCollection.UpdateOne(context.TODO(), filter, update)
+    if err != nil {
+        return domain.Task{}, err
+    }
+
+    return tr.GetByID(id)
+}
+
+func (tr *taskRepository) Delete(id string) error {
+    objectID, err := primitive.ObjectIDFromHex(id)
+    if err != nil {
+        return err
+    }
+
+    _, err = tr.taskCollection.DeleteOne(context.TODO(), bson.M{"_id": objectID})
+    return err
+}
---
+++ Clean Architecture/Repositories/userRepository.go
@@ -0,0 +1,133 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/EyasuGet/task-manager-mongo/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserRepo struct {
+	database   mongo.Database
+	collection string
+}
+
+func NewUserRepo(db mongo.Database, collection string) domain.IUserRepo {
+	return &UserRepo{
+		database:   db,
+		collection: collection,
+	}
+}
+
+func initDB(username, password string) *mongo.Collection {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	return client.Database("taskmanager").Collection("tasks")
+
+}
+
+// func (ur *UserRepo) CheckUserExists(user domain.User) (bool, domain.User) {
+// 	var result domain.User
+// 	userCollection := ur.Collection(ur.collection)
+
+// 	err := userCollection.FindOne(context.TODO(), bson.M{"email": user.Email}).Decode(&result)
+// 	return err == nil, result
+// }
+
+func (ur *UserRepo) Create(user *domain.User) (string, error) {
+
+	userCollection := ur.database.Collection(ur.collection)
+
+	// Generate a new ID for the user
+	objectID := primitive.NewObjectID()
+	user.ID = objectID.Hex()
+	_, err := userCollection.InsertOne(context.TODO(), user)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	return "user successfully registered", nil
+
+}
+
+func (ur *UserRepo) Login(usernameOrEmail, password string) (*domain.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var user domain.User
+	filter := bson.M{
+		"$or": []bson.M{
+			{"username": usernameOrEmail},
+			{"email": usernameOrEmail},
+		},
+	}
+	err := ur.database.Collection(ur.collection).FindOne(ctx, filter).Decode(&user)
+	if err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	return &user, nil
+}
+
+func (ur *UserRepo) GetByID(id primitive.ObjectID) (*domain.User, error) {
+
+	var user domain.User
+	err := ur.database.Collection(ur.collection).FindOne(context.TODO(), bson.M{"_id": id}).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (ur *UserRepo) DeleteByID(id primitive.ObjectID) error {
+	result, err := ur.database.Collection(ur.collection).DeleteOne(context.TODO(), bson.M{"_id": id})
+	if err != nil {
+		return err
+	}
+	if result.DeletedCount == 0 {
+		return errors.New("user not found")
+	}
+	return nil
+}
+
+func (ur *UserRepo) PromoteUser(userID primitive.ObjectID, newRole string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	update := bson.M{"$set": bson.M{"role": newRole}}
+	result, err := ur.database.Collection(ur.collection).UpdateByID(ctx, userID, update)
+	if err != nil {
+		return err
+	}
+	if result.MatchedCount == 0 {
+		return errors.New("user not found")
+	}
+	return nil
+}
